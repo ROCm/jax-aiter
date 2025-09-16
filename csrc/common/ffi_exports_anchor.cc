a09a2a4968519c7d57774f7b3193487d69cc469f
Ruturaj4 <Ruturaj.Vaidya@amd.com>
2025-09-16 12:45:34 -0500

    Delete dummy headers

--- csrc/common/ffi_exports_anchor.cc
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright (C) 2025, Advanced Micro Devices, Inc. All rights reserved.
-
-extern "C" void GemmA8W8(void);
-
-static void (*const keep_GemmA8W8)(void) __attribute__((used)) = GemmA8W8;
